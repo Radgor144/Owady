d0e03f5e0fa6bbdd75c13bb2fa73eafd26fbeab2
Radgor144 <106109773+Radgor144@users.noreply.github.com>
2023-06-10 21:04:12 +0200

    Add files via upload

---
+++ baza.php
@@ -0,0 +1,18 @@
+
+<?php
+        $server = "localhost";
+        $user = "";
+        $password = "";
+        $db = "";
+
+        $conn = mysqli_connect($server, $user, $password, $db);
+        
+/*
+        if(!$conn){
+            echo "Blad polaczenia z baza".mysqli_connect_end;
+        }
+        else{
+            echo "Polaczenie poprawne z baza";
+        }
+*/      
+    ?>
